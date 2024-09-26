33189e2f55606f73e76d229293f9661f1b2936b1
mprzybyl <mprzybyl@pgs-soft.com>
2024-09-26 17:50:48 +0200

    Details screen with common state

--- composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/screens/MainViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/screens/MainViewModel.kt
@@ -1,9 +1,13 @@
 package org.macpry.kmpcompose.screens
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.stateIn
 import org.koin.android.annotation.KoinViewModel
 import org.macpry.kmpcompose.managers.AppManager
@@ -13,16 +17,25 @@ class MainViewModel(
     private val appManager: AppManager
 ) : ViewModel() {
 
-    val state = appManager.timeFlow().map {
-        MainState(it.toString())
+    private var inputText by mutableStateOf("")
+
+    fun updateInput(value: String) {
+        inputText = value
+    }
+
+    val state = appManager.timeFlow().combine(
+        snapshotFlow { inputText }
+    ) { currentTime, inputText ->
+        MainState(currentTime.toString(), inputText)
     }.stateIn(
         viewModelScope,
         SharingStarted.WhileSubscribed(5000),
-        MainState(null)
+        MainState(null, null)
     )
 
 }
 
 data class MainState(
-    val currentTime: String?
+    val currentTime: String?,
+    val inputText: String?
 )
--- composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/screens/main/Main.kt
+++ composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/screens/main/Main.kt
@@ -3,36 +3,37 @@ package org.macpry.kmpcompose.screens.main
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.material.TextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import kmpcompose.composeapp.generated.resources.Res
 import kmpcompose.composeapp.generated.resources.compose_multiplatform
-import kmpcompose.composeapp.generated.resources.title_details
 import org.jetbrains.compose.resources.painterResource
-import org.jetbrains.compose.resources.stringResource
 import org.macpry.kmpcompose.Greeting
 import org.macpry.kmpcompose.screens.MainState
 
 @Composable
 fun MainScreen(
     state: MainState,
-    inputText: String,
-    onTextChanged: (String) -> Unit,
-    onOpenDetails: () -> Unit
+    navArgsInputText: String,
+    navArgsOnTextChanged: (String) -> Unit,
+    navArgsOnOpenDetails: () -> Unit,
+    commonOnTextChanged: (String) -> Unit,
+    commonOnOpenDetails: () -> Unit
 ) {
     val greeting = remember { Greeting().greet() }
     Column(
@@ -43,14 +44,31 @@ fun MainScreen(
         Text("Compose: $greeting")
         Text(state.currentTime.toString())
 
-        TextField(
-            inputText,
-            { onTextChanged(it) }
-        )
-        TextButton({
-            onOpenDetails()
-        }) {
-            Text(stringResource(Res.string.title_details))
+        Row(
+            //horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.height(50.dp).fillMaxWidth(),
+        ) {
+            TextField(
+                value = navArgsInputText,
+                onValueChange = { navArgsOnTextChanged(it) },
+                label = { Text("Input passed by nav args") },
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = { navArgsOnOpenDetails() }
+                ),
+                singleLine = true
+            )
+            //Spacer(Modifier.width(100.dp))
+            TextField(
+                value = state.inputText.orEmpty(),
+                onValueChange = { commonOnTextChanged(it) },
+                label = { Text("Input passed by view model") },
+                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = { commonOnOpenDetails() }
+                ),
+                singleLine = true
+            )
         }
         val pagerState = rememberPagerState(pageCount = { 11 })
         HorizontalPager(pagerState) { page ->
