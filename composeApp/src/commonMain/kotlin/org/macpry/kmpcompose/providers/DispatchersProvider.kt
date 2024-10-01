bd5fc0193693744589f4c1b3f3825d308e24e0c6
mprzybyl <mprzybyl@pgs-soft.com>
2024-10-01 15:30:43 +0200

    Use default dispatcher for now

--- composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/providers/DispatchersProvider.kt
+++ composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/providers/DispatchersProvider.kt
@@ -1,13 +1,25 @@
 package org.macpry.kmpcompose.providers
 
 import kotlinx.coroutines.Dispatchers
+//import kotlinx.coroutines.IO
 import org.koin.core.annotation.Named
 import org.koin.core.annotation.Single
 
 object KMPDispatchers {
+    const val DEFAULT = "DefaultDispatcher"
     const val IO = "IODispatcher"
 }
 
 @Single
+@Named(KMPDispatchers.DEFAULT)
+fun provideDefaultDispatcher() = Dispatchers.Default
+
+@Single
 @Named(KMPDispatchers.IO)
+@Deprecated(
+    "Cannot import kotlinx.coroutines.IO",
+    ReplaceWith("KMPDispatchers.DEFAULT", "org.macpry.kmpcompose.providers")
+)
+
+//TODO Investigate why cannot import kotlinx.coroutines.IO - it's not event included in sources
 fun provideIODispatcher() = Dispatchers.Default
