87ad9410c238b513a51bc86151a7f97d78e3f17b
mprzybyl <mprzybyl@pgs-soft.com>
2024-10-10 16:28:46 +0200

    Rollback kotlin update because iOS crashes

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,10 +1,9 @@
 import SwiftUI
+import ComposeApp
 
 @main
 struct iOSApp: App {
-    init() {
-        KoinKt.initKoin()
-    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
