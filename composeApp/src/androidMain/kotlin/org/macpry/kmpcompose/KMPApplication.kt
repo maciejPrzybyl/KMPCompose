50c42cf534e3dc9cbae7ee14553b971a72fdae65
mprzybyl <mprzybyl@pgs-soft.com>
2024-10-11 09:24:00 +0200

    Setup koin logger

--- composeApp/src/androidMain/kotlin/org/macpry/kmpcompose/KMPApplication.kt
+++ composeApp/src/androidMain/kotlin/org/macpry/kmpcompose/KMPApplication.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import org.macpry.kmpcompose.di.appModule
+import org.macpry.kmpcompose.logger.KoinLogger
 
 class KMPApplication : Application() {
 
@@ -11,6 +12,7 @@ class KMPApplication : Application() {
         super.onCreate()
 
         startKoin {
+            logger(KoinLogger())
             androidContext(this@KMPApplication)
             modules(appModule())
         }
--- composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/di/Init.kt
+++ composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/di/Init.kt
@@ -1,9 +1,11 @@
 package org.macpry.kmpcompose.di
 
 import org.koin.core.context.startKoin
+import org.macpry.kmpcompose.logger.KoinLogger
 
 fun initKoin() {
     startKoin {
+        logger(KoinLogger())
         modules(appModule())
     }
 }
