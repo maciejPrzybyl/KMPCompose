bc847b63a3ddb8d56831f4cb3ccdf042c3a184df
mprzybyl <mprzybyl@pgs-soft.com>
2024-10-11 16:44:26 +0200

    Hide keyboard on done

--- composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/screens/notes/NotesScreen.kt
+++ composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/screens/notes/NotesScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -36,13 +37,18 @@ fun NotesScreen(
         var text by rememberSaveable(stateSaver = TextFieldValue.Saver) {
             mutableStateOf(TextFieldValue(""))
         }
+        val focusManager = LocalFocusManager.current
         TextField(
             value = text,
             onValueChange = { text = it },
             label = { Text("Save something") },
             keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
             keyboardActions = KeyboardActions(
-                onDone = { saveText(text.text) }
+                onDone = {
+                    saveText(text.text)
+                    text = TextFieldValue("")
+                    focusManager.clearFocus()
+                }
             ),
         )
         LazyColumn(
