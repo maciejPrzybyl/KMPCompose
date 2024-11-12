d959025121d2e359d7ed919fe4fc714b9024b8b0
mprzybyl <mprzybyl@pgs-soft.com>
2024-11-12 15:04:12 +0100

    Import generated resources #22

--- composeApp/src/screens/Screens.kt
+++ composeApp/src/screens/Screens.kt
@@ -5,6 +5,10 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 import kotlinx.serialization.Serializable
 import org.jetbrains.compose.resources.StringResource
+import composeapp.generated.resources.Res
+import composeapp.generated.resources.home_navigation_main
+import composeapp.generated.resources.home_navigation_notes
+import composeapp.generated.resources.home_navigation_settings
 
 @Serializable
 sealed class AppNavigationRoutes {
@@ -15,8 +19,8 @@ sealed class AppNavigationRoutes {
     data class Maps(val coordinates: Coordinates) : AppNavigationRoutes()
 }
 
-enum class HomeBottomNavigation(val icon: ImageVector, val label: String/*Resource*/) {
-    Main(Icons.Default.AccountBox, "Res.string.home_navigation_main"),
-    Notes(Icons.Default.Call, "Res.string.home_navigation_notes"),
-    Settings(Icons.Default.Settings, "Res.string.home_navigation_settings")
+enum class HomeBottomNavigation(val icon: ImageVector, val label: StringResource) {
+    Main(Icons.Default.AccountBox, Res.string.home_navigation_main),
+    Notes(Icons.Default.Call, Res.string.home_navigation_notes),
+    Settings(Icons.Default.Settings, Res.string.home_navigation_settings)
 }
