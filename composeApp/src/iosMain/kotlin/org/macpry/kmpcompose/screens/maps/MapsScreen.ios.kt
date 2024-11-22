f57bd89aa0e053b5fe49a6ed35cf938db5dd317d
mprzybyl <mprzybyl@pgs-soft.com>
2024-11-22 10:21:58 +0100

    Zoom to user location on ios #26

--- composeApp/src/iosMain/kotlin/org/macpry/kmpcompose/screens/maps/MapsScreen.ios.kt
+++ composeApp/src/iosMain/kotlin/org/macpry/kmpcompose/screens/maps/MapsScreen.ios.kt
@@ -1,11 +1,20 @@
 package org.macpry.kmpcompose.screens.maps
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.UIKitView
+import kotlinx.cinterop.CValue
 import kotlinx.cinterop.ExperimentalForeignApi
+import platform.CoreLocation.CLLocationCoordinate2D
 import platform.CoreLocation.CLLocationCoordinate2DMake
 import platform.CoreLocation.CLLocationManager
 import platform.MapKit.MKCoordinateRegionMakeWithDistance
@@ -22,16 +31,39 @@ actual fun Map(mapsState: MapsState, onLocationPermissionsChanged: (Boolean) -> 
         modifier = Modifier.fillMaxSize(),
         factory = { mapView },
         update = {
-            mapView.setRegion(
-                MKCoordinateRegionMakeWithDistance(
-                    centerCoordinate = CLLocationCoordinate2DMake(
-                        mapsState.locationToZoom.latitude,
-                        mapsState.locationToZoom.longitude
-                    ),
-                    10_000.0, 10_000.0
-                ),
-                animated = true
+            mapView.zoomToLocation(
+                CLLocationCoordinate2DMake(
+                    mapsState.locationToZoom.latitude,
+                    mapsState.locationToZoom.longitude
+                )
             )
         }
+    )
+
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.TopEnd) {
+        IconButton(
+            {
+                mapView.userLocation.location?.let {
+                    mapView.zoomToLocation(it.coordinate)
+                }
+            },
+            Modifier.statusBarsPadding()
+        ) {
+            Icon(
+                imageVector = Icons.Default.LocationOn,
+                contentDescription = null
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalForeignApi::class)
+private fun MKMapView.zoomToLocation(coordinate: CValue<CLLocationCoordinate2D>) {
+    setRegion(
+        MKCoordinateRegionMakeWithDistance(
+            centerCoordinate = coordinate,
+            10_000.0, 10_000.0
+        ),
+        animated = true
     )
 }
