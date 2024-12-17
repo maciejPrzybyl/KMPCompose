7903482e772eace41a3bd8f9fd1b540f94287d23
mprzybyl <mprzybyl@pgs-soft.com>
2024-12-17 16:09:53 +0100

    Improve visibility of author text #25

--- composeApp/src/androidMain/kotlin/org/macpry/kmpcompose/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/macpry/kmpcompose/MainActivity.kt
@@ -3,9 +3,14 @@ package org.macpry.kmpcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.tooling.preview.Preview
+import org.macpry.kmpcompose.data.network.ImageResponse
+import org.macpry.kmpcompose.screens.main.ImagesState
+import org.macpry.kmpcompose.screens.main.MainScreen
+import org.macpry.kmpcompose.screens.main.MainState
 import org.macpry.kmpcompose.screens.notes.NotesScreen
 import org.macpry.kmpcompose.screens.notes.NotesState
 
@@ -19,12 +24,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Preview(showBackground = true)
 @Composable
-fun AppAndroidPreview() {
-    App()
+fun MainScreenPreview() {
+    MainScreen(MainState("12:12", ImagesState.Success(listOf(ImageResponse(1, "url", "aaaaaaaa")))), {}, {}, null, null)
 }
-
 
 @Preview(showBackground = true)
 @Composable
