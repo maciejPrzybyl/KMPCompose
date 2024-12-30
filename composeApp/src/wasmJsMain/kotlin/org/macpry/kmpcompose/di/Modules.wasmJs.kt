9e9c44606622059012dfcbd76db4c67df831d4e3
mprzybyl <mprzybyl@pgs-soft.com>
2024-12-30 14:02:00 +0100

    Inject dummy background worker #29

--- composeApp/src/wasmJsMain/kotlin/org/macpry/kmpcompose/di/Modules.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/org/macpry/kmpcompose/di/Modules.wasmJs.kt
@@ -1,9 +1,11 @@
 package org.macpry.kmpcompose.di
 
 import org.koin.core.module.dsl.singleOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
+import org.macpry.kmpcompose.services.worker.BackgroundWorker
 import org.macpry.kmpcompose.services.worker.DummyBackgroundWorker
 
 actual val workersModule = module {
-    singleOf(::DummyBackgroundWorker)
+    singleOf(::DummyBackgroundWorker) bind BackgroundWorker::class
 }
