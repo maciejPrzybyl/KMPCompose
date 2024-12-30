a0e857162dc3d8bcea87101eb4cd9f02784dc21b
mprzybyl <mprzybyl@pgs-soft.com>
2024-12-30 13:57:06 +0100

    Inject dummy background worker #29

--- composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/services/worker/BackgroundWorker.kt
+++ composeApp/src/commonMain/kotlin/org/macpry/kmpcompose/services/worker/BackgroundWorker.kt
@@ -1,6 +1,7 @@
 package org.macpry.kmpcompose.services.worker
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
 abstract class BackgroundWorker {
     abstract fun start()
@@ -10,4 +11,11 @@ abstract class BackgroundWorker {
     companion object {
         const val PROGRESS_TAG = "PROGRESS"
     }
+}
+
+class DummyBackgroundWorker : BackgroundWorker() {
+    override fun start() {}
+
+    override val progressFlow: Flow<Int> = flowOf(10)
+    override val tag: String = "DummyBackgroundWorker"
 }
--- composeApp/src/desktopMain/kotlin/org/macpry/kmpcompose/di/Modules.desktop.kt
+++ composeApp/src/desktopMain/kotlin/org/macpry/kmpcompose/di/Modules.desktop.kt
@@ -1,7 +1,9 @@
 package org.macpry.kmpcompose.di
 
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import org.macpry.kmpcompose.services.worker.DummyBackgroundWorker
 
 actual val workersModule = module {
-
+    singleOf(::DummyBackgroundWorker)
 }
