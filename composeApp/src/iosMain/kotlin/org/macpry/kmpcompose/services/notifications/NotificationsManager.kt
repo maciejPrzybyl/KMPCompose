a12e6ae2d9f50524fe455a5754c662aac4f9d011
mprzybyl <mprzybyl@pgs-soft.com>
2025-01-07 14:51:41 +0100

    Notifications manager in Kotlin #66

---
+++ composeApp/src/iosMain/kotlin/org/macpry/kmpcompose/services/notifications/NotificationsManager.kt
@@ -0,0 +1,33 @@
+package org.macpry.kmpcompose.services.notifications
+
+import kotlinx.cinterop.BetaInteropApi
+import kotlinx.cinterop.ExportObjCClass
+import platform.UserNotifications.UNNotification
+import platform.UserNotifications.UNNotificationPresentationOptionBanner
+import platform.UserNotifications.UNNotificationPresentationOptionSound
+import platform.UserNotifications.UNNotificationPresentationOptions
+import platform.UserNotifications.UNNotificationResponse
+import platform.UserNotifications.UNUserNotificationCenter
+import platform.UserNotifications.UNUserNotificationCenterDelegateProtocol
+import platform.darwin.NSObject
+
+@OptIn(BetaInteropApi::class)
+@ExportObjCClass
+class NotificationsManager : NSObject(), UNUserNotificationCenterDelegateProtocol {
+
+    override fun userNotificationCenter(
+        center: UNUserNotificationCenter,
+        willPresentNotification: UNNotification,
+        withCompletionHandler: (UNNotificationPresentationOptions) -> Unit
+    ) {
+        withCompletionHandler(UNNotificationPresentationOptionBanner or UNNotificationPresentationOptionSound)
+    }
+
+    override fun userNotificationCenter(
+        center: UNUserNotificationCenter,
+        didReceiveNotificationResponse: UNNotificationResponse,
+        withCompletionHandler: () -> Unit
+    ) {
+        withCompletionHandler()
+    }
+}
