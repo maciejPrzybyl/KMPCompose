b8d854a73b2a81a92a3dc654cf5b9f79f241314b
mprzybyl <mprzybyl@pgs-soft.com>
2025-01-23 14:07:02 +0100

    Simplify gradle setup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,15 +3,7 @@ enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
     repositories {
-        //maven("https://androidx.dev/kmp/builds/12227334/artifacts/snapshots/repository")
-        //maven("https://androidx.dev/snapshots/builds/12227334/artifacts/repository")
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
@@ -19,15 +11,7 @@ pluginManagement {
 
 dependencyResolutionManagement {
     repositories {
-        //maven("https://androidx.dev/kmp/builds/12227334/artifacts/snapshots/repository")
-        //maven("https://androidx.dev/snapshots/builds/12227334/artifacts/repository")
-        google {
-            mavenContent {
-                includeGroupAndSubgroups("androidx")
-                includeGroupAndSubgroups("com.android")
-                includeGroupAndSubgroups("com.google")
-            }
-        }
+        google()
         mavenCentral()
     }
 }
