a05f931c736e2db86ee8c6772d1939a41063b6e1
mprzybyl <mprzybyl@pgs-soft.com>
2025-01-29 15:19:38 +0100

    Show user screen for iOS #67

--- composeApp/src/iosMain/kotlin/org/macpry/kmpcompose/screens/Screens.ios.kt
+++ composeApp/src/iosMain/kotlin/org/macpry/kmpcompose/screens/Screens.ios.kt
@@ -3,5 +3,6 @@ package org.macpry.kmpcompose.screens
 actual val bottomNavigationItems = listOf(
     HomeBottomNavigation.Main,
     HomeBottomNavigation.Notes,
-    HomeBottomNavigation.Settings
+    HomeBottomNavigation.Settings,
+    HomeBottomNavigation.User
 )
